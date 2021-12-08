1fc58f7e22d969c09c763c71c55aaa83f62ebe4d
Tobias Goldkette Prang <58465104+TGPGamez@users.noreply.github.com>
2021-12-08 10:27:22 +0100

    Comments

--- HelpJakob-H2/HTMLConverter.cs
+++ HelpJakob-H2/HTMLConverter.cs
@@ -8,6 +8,11 @@ namespace HelpJakob_H2
 {
     public static class HTMLConverter
     {
+        /// <summary>
+        /// Used to convert a plain text to HTML
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns></returns>
         public static string ConvertBodyToHTML(string plainText)
         {
             return "" + plainText + "";
--- HelpJakob-H2/MessageSender.cs
+++ HelpJakob-H2/MessageSender.cs
@@ -8,38 +8,44 @@ namespace HelpJakob_H2
 {
     public class MessageSender
     {
+        /// <summary>
+        /// Used to send message to recipient 
+        /// </summary>
         public void sendMessage(MessageCarrier type, Message m, bool isHTML)
         {
-            //herinde sendes der en email ud til modtageren
+            //Sends out a email to receiver
             if (type.Equals(MessageCarrier.Smtp))
             {
                 if (isHTML)
                     m.Body = HTMLConverter.ConvertBodyToHTML(m.Body);
-                //her implementeres alt koden til at sende via Smtp
+                //Implement code to send with Smtp
             }
 
             if (type.Equals(MessageCarrier.VMessage))
             {
                 if (isHTML)
                     m.Body = HTMLConverter.ConvertBodyToHTML(m.Body);
-                //her implementeres alt koden til at sende via VMessage
+                //Implement code to send with Smtp VMessage
             }
         }
 
+        /// <summary>
+        /// Used to send a message to all 
+        /// </summary>
         public void sendMessageToAll(MessageCarrier type, string[] to, Message m, bool isHTML)
         {
             if (type.Equals(MessageCarrier.Smtp))
             {
                 if (isHTML)
                     m.Body = HTMLConverter.ConvertBodyToHTML(m.Body);
-                //her implementeres alt koden til at sende via Smtp
+                //Implement code to send with Smtp
             }
 
             if (type.Equals(MessageCarrier.VMessage))
             {
                 if (isHTML)
                     m.Body = HTMLConverter.ConvertBodyToHTML(m.Body);
-                //her implementeres alt koden til at sende via VMessage
+                //Implement code to send with Smtp VMessage
             }
         }
     }
